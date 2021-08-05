bb98ce9b4009197d0ce249a52ce359ca185b6397
ahadihjosue <josueahadihabib@gmail.com>
2021-08-05 15:35:26 +0200

    First Commit

---
+++ js/preloader.js
@@ -0,0 +1,14 @@
+/**********************   PRELOADER  ****************************/ 
+
+
+$(window).on("load", function(){
+    $("#loader-wrapper").fadeOut("slow");
+});
+
+//Without using jquery here is my custom javascript, if you don't like plugins, this javascript is at work
+
+//var preloader = document.getElementById('loader-wrapper');
+
+//function load() {
+///   preloader.style.display = 'none';
+//}
